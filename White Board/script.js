a4fe7b0ecdf92bcbfc38d9616be326694a3688da
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-27 15:46:51 +0530

    ccccc

--- White Board/script.js
+++ White Board/script.js
@@ -330,3 +330,17 @@ let downloadDiv = document.querySelector("#download");
 photoDiv.addEventListener("click", function() {
 	photoUploadInput.click();
 });
+
+
+photoUploadInput.addEventListener("change", function(event) {
+	console.log(event);
+	let fileObj = event.target.files[0];
+	console.log(fileObj);
+	let filePath = URL.createObjectURL(fileObj, {
+		type: "image/jpg"
+	});
+	let img = document.createElement("img");
+	img.setAttribute("src", filePath);
+	img.classList.add("sticky-image");
+	addSticky(img);
+});
