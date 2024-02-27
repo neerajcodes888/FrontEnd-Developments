0bbc5ec8ae5adbdbb88e282d4e7a8e9a87693504
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-02-27 15:46:04 +0530

    cccccc

--- White Board/script.js
+++ White Board/script.js
@@ -264,3 +264,30 @@ function undoLine() {
 		drawLinesFromDB();
 	}
 }
+
+
+function redoLine() {
+	if (redoLinesDB.length) {
+
+		let currentLineWidth = ctx.lineWidth;
+		let currentStrokeStyle = ctx.strokeStyle;
+
+		let redoLine = redoLinesDB.pop();
+		for (let i = 0; i < redoLine.length; i++) {
+			let pointObject = redoLine[i];
+			if (pointObject.type == "md") {
+				ctx.lineWidth = pointObject.lineWidth;
+				ctx.strokeStyle = pointObject.strokeStyle;
+				ctx.beginPath();
+				ctx.moveTo(pointObject.x, pointObject.y);
+			} else {
+				ctx.lineTo(pointObject.x, pointObject.y);
+				ctx.stroke();
+			}
+		}
+		linesDB.push(redoLine);
+
+		ctx.lineWidth = currentLineWidth;
+		ctx.strokeStyle = currentStrokeStyle;
+	}
+}
