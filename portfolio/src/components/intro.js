2842aa208613b2db259d2c0232bee9806baaf8c6
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-03 10:12:28 +0000

    cccc

--- portfolio/src/components/intro.js
+++ portfolio/src/components/intro.js
@@ -3,3 +3,31 @@ import styled from 'styled-components'
 import {motion} from 'framer-motion'
 import Me from '../assets/Images/profile-img.png'
 
+const Box = styled(motion.div)`
+
+position: absolute;
+left: 50%;
+top: 50%;
+transform: translate(-50%, -50%);
+
+width: 65vw;
+height:55vh;
+display: flex;
+
+background: linear-gradient(
+    to right,
+    ${props => props.theme.body} 50%,
+    ${props => props.theme.text} 50%) bottom,
+    linear-gradient(
+    to right,
+    ${props => props.theme.body} 50%,
+    ${props => props.theme.text} 50%) top;
+    background-repeat: no-repeat;
+background-size: 100% 2px;
+    border-left: 2px solid ${props => props.theme.body};
+    border-right: 2px solid ${props => props.theme.text};
+
+
+    z-index:1;
+
+`
