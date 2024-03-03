a2d03c197cd6b1c020c25d559a6b6cb30e368bcd
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-03 10:29:50 +0000

    Final Commit

--- portfolio/src/components/themes.js
+++ portfolio/src/components/themes.js
@@ -6,3 +6,14 @@ export const lightTheme = {
     bodyRgba : "252, 246, 244",
     textRgba:"0,0,0",
 }
+
+
+
+export const DarkTheme = {
+
+    body:"#000000",
+    text:"#FCF6F4",
+    fontFamily:"'Source Sans Pro', sans-serif",
+    textRgba : "252, 246, 244",
+    bodyRgba:"0,0,0",
+}
