9c1efb9be25ef03eb10ecbded147d8ce41990d89
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-04 06:05:06 +0000

    cccc

--- portfolio/src/components/AboutPage.js
+++ portfolio/src/components/AboutPage.js
@@ -9,3 +9,11 @@ import PowerButton from '../subComponents/PowerButton';
 import ParticleComponent from '../subComponents/ParticleComponent';
 import BigTitle from '../subComponents/BigTitlte'
 import astronaut from '../assets/Images/spaceman.png'
+
+const Box = styled.div`
+background-color: ${props => props.theme.body};
+width: 100vw;
+height:100vh;
+position: relative;
+overflow: hidden;
+`
