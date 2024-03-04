0a899ef79c63346f2045688d2293dabdb2f3e1f1
Neeraj Kumar <98253646+neerajcodes888@users.noreply.github.com>
2024-03-04 06:04:48 +0000

    cccc

--- portfolio/src/components/AboutPage.js
+++ portfolio/src/components/AboutPage.js
@@ -3,3 +3,9 @@ import styled, { keyframes, ThemeProvider } from 'styled-components'
 import {DarkTheme} from './Themes';
 
 
+import LogoComponent from '../subComponents/LogoComponent';
+import SocialIcons from '../subComponents/SocialIcons';
+import PowerButton from '../subComponents/PowerButton';
+import ParticleComponent from '../subComponents/ParticleComponent';
+import BigTitle from '../subComponents/BigTitlte'
+import astronaut from '../assets/Images/spaceman.png'
